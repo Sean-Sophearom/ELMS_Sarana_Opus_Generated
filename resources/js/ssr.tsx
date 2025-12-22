bd67bf9ec0a31ae834ef07be3a3140bbe51ae54d
Sean Sophearom <phearom@niy.ai>
2025-12-22 09:27:07 +0700

    add ssr

---
+++ resources/js/ssr.tsx
@@ -0,0 +1,27 @@
+import { createInertiaApp } from '@inertiajs/react';
+import createServer from '@inertiajs/react/server';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import ReactDOMServer from 'react-dom/server';
+import { route } from 'ziggy-js';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createServer((page) =>
+    createInertiaApp({
+        page,
+        render: ReactDOMServer.renderToString,
+        title: (title) => `${title} - ${appName}`,
+        resolve: (name) =>
+            resolvePageComponent(
+                `./Pages/${name}.tsx`,
+                import.meta.glob('./Pages/**/*.tsx'),
+            ),
+        setup: ({ App, props }) => {
+            // @ts-expect-error - Ziggy config from page props
+            globalThis.route = (name, params, absolute) =>
+                // @ts-expect-error - Ziggy config from page props
+                route(name, params, absolute, props.initialPage.props.ziggy);
+            return <App {...props} />;
+        },
+    }),
+);
